a36c575572e7cc2b76de5454072103f4bbbeb82b
Zachary White <zfwhite91@gmail.com>
2017-11-24 14:30:29 -0800

    gotta have an overlay

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -1,2 +1,5 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+$(document).on('click', '.cancel-new-post', function() {
+  $('#overlay').hide();
+});
